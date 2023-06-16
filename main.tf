2e8a283b395cb83ae7288d9f657362e436ce0d65
Sohail Mohammed <cloudtraining44@gmail.com>
2023-06-16 00:11:52 -0500

    Added provider block

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+ provider "aws" {
+  region = "us-east-1"
+}
+ 
  resource "aws_vpc" "main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
    instance_tenancy = "default"
